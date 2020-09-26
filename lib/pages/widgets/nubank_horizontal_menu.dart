5ed099d7ad2fb345b4a7bb4e36e8d98a7340457f
FallenHell <gabriel.ferreri98@gmail.com>
2020-09-26 15:53:58 -0500

    menu horizontal

--- lib/pages/widgets/nubank_horizontal_menu.dart
+++ lib/pages/widgets/nubank_horizontal_menu.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import '../../data.dart';
 
 class HorizontalMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height * 0.25,
+      height: 100,
       color: Colors.white,
+      child: ListView.separated(
+          separatorBuilder: (context, index) => Container(
+                width: 10,
+                color: Theme.of(context).primaryColorDark,
+              ),
+          itemCount: data['menu_order'].length,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => _FeatureItem(
+              data['menu_order'][index], Icons.person_add,
+              onClick: null)
+          // _FeatureItem('Indicar Amigos', Icons.person_add, onClick: null),
+          // _FeatureItem('Pix', Icons.monetization_on, onClick: null),
+          // _FeatureItem('Pagar', Icons.payment, onClick: null),
+          // _FeatureItem('Transferir', FontAwesome5.comment_dollar,
+          //     onClick: null),
+          // _FeatureItem('Depositar', FontAwesome5.comments_dollar, onClick: null)
+          //],
+          ),
     );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {@required this.onClick})
+      : assert(icon != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+            onTap: () => onClick(),
+            child: Container(
+                padding: EdgeInsets.all(8.0),
+                width: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      icon,
+                      color: Colors.white,
+                      size: 32.0,
+                    ),
+                    Text(
+                      name,
+                      style: TextStyle(color: Colors.white, fontSize: 16.0),
+                    )
+                  ],
+                ))));
   }
 }
