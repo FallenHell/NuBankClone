40c7efa98ba20e2e13d2243a85f653a1e5ea5e1f
Rodrigo Dias <rod90ad@gmail.com>
2020-09-26 16:08:12 -0300

    subindo alterações

---
+++ lib/data.dart
@@ -0,0 +1,6 @@
+Map<String, dynamic> data = {
+  "name": "FellHell",
+  "agency": "0001",
+  "account": "999999-2",
+  "balance": "280,00"
+};
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,67 @@
+import 'package:NuBankClone/pages/root_page.dart';
+import 'package:flutter/material.dart';
+import '../data.dart';
+
+class SplashNubank extends StatefulWidget {
+  @override
+  _SplashNubankState createState() => _SplashNubankState();
+}
+
+class _SplashNubankState extends State<SplashNubank>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _expandName;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        value: 0, vsync: this, duration: Duration(milliseconds: 350));
+    _expandName = Tween<double>(begin: 0, end: 1).animate(_controller);
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed)
+        Future.delayed(Duration(milliseconds: 500), () {
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => RootPage()));
+        });
+    });
+    _controller.addListener(() {
+      setState(() {});
+    });
+    Future.delayed(Duration(milliseconds: 1000), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Theme.of(context).primaryColor,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 80,
+                height: 80,
+                child: Image.asset("images/logo.png", color: Colors.white),
+              ),
+              SizedBox(width: 15),
+              SizedBox(
+                width: _expandName.value * 100,
+                child: Text(
+                  data["name"],
+                  maxLines: 1,
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  overflow: TextOverflow.clip,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
